2c5e05f7517cff3cac3f9caefdabeae2a71c5e4f
Jeff-Maina <devyoungblud@gmail.com>
2024-02-01 02:48:51 +0300

    Add buttons page

---
+++ src/app/(pages)/buttons/page.tsx
@@ -0,0 +1,14 @@
+const Buttons = () => {
+  return (
+    <main className="p-6 pt-14">
+      <h1 className="font-campton-bold text-4xl text-[#111]">Buttons</h1>
+      <br />
+      <br />
+      <section>
+        <article className="w-full aspect-square border bg-zinc-100 rounded-lg"></article>
+      </section>
+    </main>
+  );
+};
+
+export default Buttons;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,19 @@ import { Elements, Sections } from "@/data/AppData";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col gap-16">
-      <header className="h-[60vh] flex flex-col items-center justify-center">
+  return ( <main className="flex flex-col gap-16">
+      <header className="h-[35vh] flex flex-col items-center justify-center">
         <p className="text-[8rem] md:text-[11rem] lg:text-[15rem] tracking-tighter font-campton-bold text-center ">
           craft.
         </p>
-        <p className="font-graphik-semibold text-zinc-700 text-center md:text-xl max-w-xs w-full">
+        <p className="font-satoshi-medium text-zinc-800 text-center md:text-xl max-w-xs w-full">
           A collection of ui components for{" "}
-          <span className="bg-gradient-to-r from-blue-900 to-blue-500 text-transparent bg-clip-text">
-            React
-          </span>{" "}
-          , <span className="text-[#44bdf8]">Taiwind css </span> and
+          {/* <span className="bg-gradient-to-r from-blue-900 to-blue-500 text-transparent bg-clip-text"> */}
+          React
+          {/* </span>{" "} */}
+          {/* , <span className="text-[#44bdf8]"> */}
+          Taiwind css
+          {/* </span> */} and
           <span className="bg-gradient-to-r text-transparent bg-clip-text from-blue-700 via-blue-500 to-pink-500">
             {" "}
             Framer motion
@@ -29,14 +30,14 @@ export default function Home() {
         <br />
         <section className="flex flex-col gap-3">
           {Elements.map((element, index) => (
-            <div className="h-36 apsect-video border border-zinc-200 rounded p-4 flex flex-col justify-end font-graphik-semibold">
+            <a href={element.elementPagePath} className="h-36 apsect-video border border-zinc-200 rounded p-4 flex flex-col justify-end font-graphik-semibold">
               <p className="text-[#111]">
                 {element.elementName}{" "}
                 <sup className="text-zinc-500 font-campton-bold text-sm">
                   {element.componentCount}
                 </sup>{" "}
               </p>
-            </div>
+            </a>
           ))}{" "}
         </section>
       </section>
