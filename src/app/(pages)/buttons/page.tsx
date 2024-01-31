d3b0fbedbffdfb79fcab0403a5e7bdf2c5cfc1ff
Jeff-Maina <devyoungblud@gmail.com>
2024-02-01 02:59:27 +0300

    Add component count to buttons page

--- src/app/(pages)/buttons/page.tsx
+++ src/app/(pages)/buttons/page.tsx
@@ -1,7 +1,13 @@
 const Buttons = () => {
   return (
-    <main className="p-6 pt-14">
-      <h1 className="font-satoshi-bold text-5xl text-[#111] tracking-tighter">Buttons.</h1>
+    <main className="p-6 pt-10">
+      <header className="flex flex-col gap-6">
+        <p className="font-satoshi-medium">Elements / Buttons</p>
+        <h1 className="font-satoshi-bold text-5xl text-[#111] tracking-tighter">
+          Buttons.
+          <sup className="text-4xl text-zinc-400">18</sup>
+        </h1>
+      </header>
       <br />
       <br />
       <section>
