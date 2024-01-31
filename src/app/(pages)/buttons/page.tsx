a684f464d2b2001d28db8f559e55c543c7ee3893
Jeff-Maina <devyoungblud@gmail.com>
2024-02-01 02:53:51 +0300

    Add buttons page

--- src/app/(pages)/buttons/page.tsx
+++ src/app/(pages)/buttons/page.tsx
@@ -1,11 +1,11 @@
 const Buttons = () => {
   return (
     <main className="p-6 pt-14">
-      <h1 className="font-campton-bold text-4xl text-[#111]">Buttons</h1>
+      <h1 className="font-satoshi-bold text-5xl text-[#111] tracking-tighter">Buttons.</h1>
       <br />
       <br />
       <section>
-        <article className="w-full aspect-square border bg-zinc-100 rounded-lg"></article>
+        <article className="w-full aspect-square border bg-zinc-100 rounded-xl"></article>
       </section>
     </main>
   );
