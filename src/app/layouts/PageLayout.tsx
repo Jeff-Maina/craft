635b1ee7e52e38c2e0706122132236ec8fd138d0
Jeff-Maina <devyoungblud@gmail.com>
2024-02-01 19:37:48 +0300

    Make landing page responsive for bigger screens.

--- src/app/layouts/PageLayout.tsx
+++ src/app/layouts/PageLayout.tsx
@@ -23,17 +23,19 @@ const PageLayout: FC<ComponentProps> = ({ children, pageOptions }) => {
       : "/";
 
   return (
-    <main className="p-6 pt-10 flex flex-col min-h-screen">
-      <header className="flex flex-col gap-4">
-        <div className="font-satoshi-medium flex items-center gap-3">
+    <main className="p-6 pt-10 flex flex-col min-h-screen max-w-7xl m-auto">
+      <header className="flex flex-col gap-4 lg:gap-6">
+        <div className="font-satoshi-medium flex items-center gap-3 md:text-lg">
           <Link href={`/${path}`}>{category}</Link>{" "}
-          <span>{!isMainPage && "/"}</span>
+          <small>{!isMainPage && "/"}</small>
           {!isMainPage && page}
         </div>
-        <h1 className="font-satoshi-bold text-5xl text-[#111] tracking-tighter">
-          {page}.<sup className="text-4xl text-zinc-400">{componentCount}</sup>
+        <h1 className="font-satoshi-bold text-5xl md:text-6xl lg:text-8xl selection:bg-black selection:text-white text-[#111] tracking-tighter">
+          {page}.<sup className="text-4xl md:text-5xl text-zinc-400">{componentCount}</sup>
         </h1>
-        <p  className="font-satoshi-medium text-zinc-500">Just copy and paste 😍</p>
+        <p className="font-satoshi-medium text-zinc-500 md:text-lg lg:text-xl">
+          Just copy and paste 😍
+        </p>
       </header>
       <br />
       <br />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col gap-16">
-      <header className="h-[50vh] flex flex-col items-center justify-center">
+      <header className="h-[50vh] lg:h-[70vh] flex flex-col items-center justify-center">
         <p className="text-[8rem] md:text-[11rem] lg:text-[15rem] tracking-tighter font-campton-bold text-center ">
           craft.
         </p>
-        <p className="font-satoshi-medium text-zinc-800 text-center md:text-xl max-w-xs w-full">
+        <p className="font-satoshi-medium text-zinc-800 text-center md:text-xl max-w-xs md:max-w-sm w-full">
           A collection of ui components for{" "}
           {/* <span className="bg-gradient-to-r from-blue-900 to-blue-500 text-transparent bg-clip-text"> */}
           React
           {/* </span>{" "} */}
-          {/* , <span className="text-[#44bdf8]"> */}
-          Taiwind css
+          {/* , <span className="text-[#44bdf8]"> */}, Tailwind css
           {/* </span> */} and
           <span className="bg-gradient-to-r text-transparent bg-clip-text from-blue-700 via-blue-500 to-pink-500">
             {" "}
@@ -23,20 +22,20 @@ export default function Home() {
           .
         </p>
       </header>
-      <section className="p-6">
-        <h1 className="text-center font-graphik-semibold text-2xl tracking-tight">
+      <section className="p-6 max-w-7xl m-auto w-full">
+        <h1 className="text-center font-graphik-semibold text-2xl tracking-tight lg:text-4xl">
           Browse elements.
         </h1>
         <br />
         <br />
-        <section className="flex flex-col gap-3">
+        <section className="grid gap-3 md:grid-cols-2 lg:gap-6">
           {ElementsData.map((element, index) => (
             <Link href={element.elementPagePath}>
               <div
                 key={index}
-                className="h-36 apsect-video border border-zinc-200 rounded p-4 flex flex-col justify-end font-graphik-semibold"
+                className="h-36 md:h-44 lg:h-96 w-full lg:aspect-video border border-zinc-300 rounded-xl p-4 lg:p-6 flex flex-col justify-end font-satoshi-medium hover:border-black transition-all duration-300"
               >
-                <p className="text-[#111]">
+                <p className="text-[#333] lg:text-xl">
                   {element.elementName}{" "}
                   <sup className="text-zinc-500 font-campton-bold text-sm">
                     {element.componentCount}
@@ -47,17 +46,21 @@ export default function Home() {
           ))}{" "}
         </section>
       </section>
-      <section className="p-6">
-        <h1 className="text-center font-graphik-semibold text-2xl tracking-tight">
+      <section className="p-6 max-w-7xl m-auto w-full">
+        <h1 className="text-center font-graphik-semibold text-2xl tracking-tight lg:text-4xl">
           Browse sections
         </h1>
         <br />
         <br />
-        <section className="flex flex-col gap-3">
+        <section className="grid gap-3 md:grid-cols-2 lg:gap-6">
           {SectionsData.map((section, index) => (
             <Link href={section.sectionPagePath}>
-              <div className="h-36 apsect-video border border-zinc-200 rounded p-4 flex flex-col justify-end font-graphik-semibold">
-                <p className="text-[#111]">
+              <div
+                key={index}
+                className="h-36 md:h-44 lg:h-96 w-full lg:aspect-video border border-zinc-300 rounded-xl p-4 lg:p-6 flex flex-col justify-end font-satoshi-medium hover:border-black transition-all duration-300"
+              >
+                
+                <p className="text-[#333] lg:text-xl">
                   {section.sectionName}{" "}
                   <sup className="text-zinc-500 font-campton-bold text-sm">
                     {section.componentCount}
