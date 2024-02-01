46ac3ba3a890d35144dc72e9379feaa718bad5a9
Jeff-Maina <devyoungblud@gmail.com>
2024-02-01 13:12:26 +0300

    Add github link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="p-6 flex items-center justify-between font-satoshi-medium text-lg text-black/50" >
+    <footer className="p-6 flex items-center justify-between font-satoshi-medium text-sm text-black/50" >
       <p className="">© 2024 Craft</p>
-      <p>Created by <a href="/" className="text-black underline">Jeff</a></p>
+      <p></p>
+      <p>Created by <a href="https://github.com/Jeff-Maina" target="_blank" className="text-black underline">Jeff</a></p>
     </footer>
   );
 };
