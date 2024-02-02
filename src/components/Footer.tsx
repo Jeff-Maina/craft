446178fe8ef1f5731ab9f4cd6ccba720072f03ef
Jeff-Maina <devyoungblud@gmail.com>
2024-02-02 23:40:34 +0300

    Increase component card height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="font-satoshi-medium text-sm text-black/50 lg:px-24 md:text-lg m-auto lg:mt-20 border-t border-zinc-200/60 grid grid-cols-2 divide-x divide-zinc-200/60 leading-none">
+    <footer className="font-satoshi-medium text-sm text-black/50 lg:px-24 md:text-lg m-auto mt-10 lg:mt-20 border-t border-zinc-200/60 grid grid-cols-2 divide-x divide-zinc-200/60 leading-none">
       <div className="w-full text-center p-6">
         <p className="">© 2024 Craft</p>
       </div>
