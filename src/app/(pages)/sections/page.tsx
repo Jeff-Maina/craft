71ea8c7035ae672bccf69dfd5861d7f974210d8b
Jeff-Maina <devyoungblud@gmail.com>
2024-02-05 21:47:47 +0300

    Remove unrequired directories

--- src/app/(pages)/sections/page.tsx
+++ src/app/(pages)/sections/page.tsx
@@ -1,9 +1,11 @@
+// component imports;
 import PageLayout from "@/app/layouts/PageLayout";
-import { SectionsData } from "@/data/AppData";
-
-import type { Pageprops } from "@/lib/Interfaces";
 import ComponentCard from "@/app/layouts/Cards/ComponentCard";
 import PageHeader from "../(components)/PageHeader";
+// types and interfaces;
+import type { Pageprops } from "@/lib/Interfaces";
+// data
+import { SectionsData } from "@/data/AppData";
 
 const PageOptions: Pageprops = {
   page: "Sections",
@@ -19,37 +21,10 @@ const Sections = () => {
         <br />
         <div className="grid w-full  border  border-zinc-200/60 lg:border-zinc-200 divide-y divide-zinc-200/60 lg:divide-zinc-200">
           <ComponentCard
-            componentName="FAQs"
-            componentCount={0}
-            isSection={true}
-            pagePath="/sections/faqs"
-            className="xl:h-[80vh]"
-          >
-            <div></div>
-          </ComponentCard>
-          <ComponentCard
-            componentName="Hero sections"
-            componentCount={0}
-            isSection={true}
-            pagePath="/sections/hero_sections"
-            className="xl:h-[80vh]"
-          >
-            <div></div>
-          </ComponentCard>
-          <ComponentCard
-            componentName="Footer"
-            componentCount={0}
-            isSection={true}
-            pagePath="/sections/footer"
-            className="xl:h-[80vh]"
-          >
-            <div></div>
-          </ComponentCard>
-          <ComponentCard
             componentName="Navbars"
             componentCount={0}
             isSection={true}
-            pagePath="/sections/navbar"
+            pagePath="/sections/navbars"
             className="xl:h-[80vh]"
           >
             <div></div>
--- src/data/AppData.tsx
+++ src/data/AppData.tsx
@@ -41,26 +41,6 @@ export const SectionsData: Array<SectionProps> = [
     component_path: "/sections/navbars",
     component_count: 0,
   },
-  {
-    component_name: "Testimonials",
-    component_path: "/sections/testimonials",
-    component_count: 0,
-  },
-  {
-    component_name: "FAQS",
-    component_path: "/sections/FAQs",
-    component_count: 0,
-  },
-  {
-    component_name: "Contact",
-    component_path: "/sections/contact",
-    component_count: 0,
-  },
-  {
-    component_name: "Footer",
-    component_path: "/sections/footer",
-    component_count: 0,
-  },
 ];
 
 export const ElementsData: Array<ElementProps> = [
@@ -68,17 +48,7 @@ export const ElementsData: Array<ElementProps> = [
     component_name: "Buttons",
     component_path: "/elements/buttons",
     component_count: 0,
-  },
-  {
-    component_name: "Links",
-    component_path: "/elements/links",
-    component_count: 0,
-  },
-  {
-    component_name: "Text",
-    component_path: "/elements/text",
-    component_count: 0,
-  },
+  }
 ];
 
 export const FaqsList: Array<faqsProps> = [
