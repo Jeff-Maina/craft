39254a628ed9321660962246201b537265109fbd
Jeff-Maina <devyoungblud@gmail.com>
2024-02-06 23:12:44 +0300

    Remove border and add bg color instead

--- src/app/(pages)/elements/ElementLayout.tsx
+++ src/app/(pages)/elements/ElementLayout.tsx
@@ -28,7 +28,7 @@ const ElementLayout: FC<ButtonProps> = ({ children, codeBlock, className }) => {
 
   return (
     <article
-      className={`w-full aspect-square  flex flex-col justify-between pt-4 ${className} font-satoshi-medium group/card relative border border-zinc-200/60 md:border-zinc-200 hover:border-zinc-300 transition-all duration-200`}
+      className={`w-full h-64 md:h-auto md:aspect-square  flex flex-col justify-between pt-4 ${className} font-satoshi-medium group/card relative bg-zinc-100/80 border-zinc-200/60 md:border-zinc-200 hover:lg:border-zinc-300 transition-all duration-200`}
     >
       <div className="p-4 w-full h-full flex items-center justify-center">
         {children}
--- src/app/(pages)/elements/buttons/(examples)/button_001/Button_001.tsx
+++ src/app/(pages)/elements/buttons/(examples)/button_001/Button_001.tsx
@@ -4,12 +4,12 @@ import { codeblock } from "./codeblock";
 const Button001 = () => {
   return (
     <ElementLayout codeBlock={codeblock}>
-      <button className="relative px-8 py-3 group/button outline-blue-500">
+      <button className="relative px-8 py-3 group/button outline-blue-500 isolate">
         <span className="uppercase  text-sm md:text-base font-satoshi-medium text-white group-hover/button:text-black transition-colors duration-300">
           hover me
         </span>
         <div className="absolute -z-10 inset-0 w-full h-full bg-[#2a2a2a] opacity-1 group-hover/button:opacity-0 scale-100 group-hover/button:scale-0 transition-all duration-500"></div>
-        <div className="absolute -z-[5] inset-0 w-full h-full border border-[#2a2a2a] scale-125 opacity-0 group-hover/button:scale-100 group-active/button:scale-105 group-hover/button:opacity-100 transition-all duration-300"></div>
+        <div className="absolute -z-[5] inset-0 w-full h-full bg-white border border-[#2a2a2a] scale-125 opacity-0 group-hover/button:scale-100 group-active/button:scale-105 group-hover/button:opacity-100 transition-all duration-300"></div>
       </button>
     </ElementLayout>
   );
