c0c503c9b471b6a9f161d3e49744c9717a623cba
Jeff-Maina <devyoungblud@gmail.com>
2024-02-08 22:18:12 +0300

    add link 4

---
+++ src/app/(pages)/elements/links/(examples)/link_003/link_003.tsx
@@ -0,0 +1,25 @@
+import { ArrowRight } from "lucide-react";
+import ElementLayout from "../../../ElementLayout";
+import { codeblock } from "./codeblock";
+import { FC } from "react";
+
+const ArrowSvg = () => (
+  <ArrowRight className="h-[14px] md:h-[16px] w-[14px] md:w-[16px] stroke-white" />
+);
+
+const Link003: FC = () => {
+  return (
+    <ElementLayout codeBlock={codeblock}>
+      <a href="#" className="group/link relative max-w-fit overflow-hidden">
+        <span className="lg:text-xl">example@design.com</span>
+        <div className="absolute bottom-0 h-[1px] w-full bg-black -translate-x-[250%]  group-hover/link:translate-x-0 transition-all duration-700 ease-in-out"></div>
+        <div className="absolute bottom-0 h-[1px] w-full bg-black group-hover/link:translate-x-[250%]  transition-all duration-700 ease-in-out"></div>
+      </a>
+    </ElementLayout>
+  );
+};
+
+export default Link003;
+
+// feb 6 1:44
+// feb 6 1:46
--- src/app/(pages)/elements/links/Linkslist.tsx
+++ src/app/(pages)/elements/links/Linkslist.tsx
@@ -1,6 +1,13 @@
+import Link003 from "./(examples)/link_003/link_003";
+import Link004 from "./(examples)/link_004/link_004";
 import Link01 from "./(examples)/link_01/Link01";
 import Link02 from "./(examples)/link_02/link_02";
 
 type LinkProps = JSX.Element;
 
-export const Linklist: Array<LinkProps> = [<Link01 />, <Link02 />];
+export const Linklist: Array<LinkProps> = [
+  <Link01 />,
+  <Link02 />,
+  <Link003 />,
+  <Link004 />,
+];
