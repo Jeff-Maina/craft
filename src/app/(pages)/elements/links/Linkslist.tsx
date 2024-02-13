01755a53eb1a25737354632800ac09e6ff4d49c4
Jeff-Maina <devyoungblud@gmail.com>
2024-02-13 21:32:54 +0300

    Add link component 009

--- src/app/(pages)/elements/links/Linkslist.tsx
+++ src/app/(pages)/elements/links/Linkslist.tsx
@@ -6,6 +6,7 @@ import Link02 from "./(examples)/link_002/link_002";
 import Link006 from "./(examples)/link_006/link_006";
 import Link007 from "./(examples)/link_007/Link_007";
 import Link008 from "./(examples)/link_008/Link_008";
+import Link009 from "./(examples)/link_009/link_009";
 
 type LinkProps = JSX.Element;
 
@@ -17,5 +18,6 @@ export const Linklist: Array<LinkProps> = [
   <Link005 />,
   <Link006 />,
   <Link007 />,
-  <Link008 />
+  <Link008 />,
+  <Link009 />,
 ];
