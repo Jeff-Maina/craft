7192fba2299582735502f9de3e639649c04c1f19
Jeff-Maina <devyoungblud@gmail.com>
2024-02-15 10:41:21 +0300

    Add button component 008

--- src/app/(pages)/elements/buttons/(examples)/button_008/button_008.tsx
+++ src/app/(pages)/elements/buttons/(examples)/button_008/button_008.tsx
@@ -1,10 +1,51 @@
+"use client";
+
+import { useEffect, useRef } from "react";
 import ElementLayout from "../../../ElementLayout";
 import { codeblock } from "./codeblock";
+import { gsap } from "gsap";
 
 const Button008 = () => {
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    const xTo = gsap.quickTo(buttonRef.current, "x", {
+      duration: 1,
+      ease: "elastic.out(1,0.3)",
+    });
+    const yTo = gsap.quickTo(buttonRef.current, "y", {
+      duration: 1,
+      ease: "elastic.out(1,0.3)",
+    });
+
+    buttonRef.current?.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      const { top, left, height, width } =
+        buttonRef.current.getBoundingClientRect();
+      const x = clientX - (left + width / 2);
+      const y = clientY - (top + height / 2);
+
+      xTo(x * 0.5);
+      yTo(y * 0.7);
+    });
+
+    buttonRef.current?.addEventListener("mouseleave", () => {
+      xTo(0);
+      yTo(0);
+    });
+  }, []);
+
   return (
     <ElementLayout codeBlock={codeblock}>
-      <button></button>
+      <button className="relative isolate">
+        <div
+          ref={buttonRef}
+          className="w-36 lg:w-44 h-14 rounded-full border border-[#2a2a2a] bg-white grid place-items-center font-graphik-semibold lg:text-xl text-[#2a2a2a]"
+        >
+          Join us
+        </div>
+        <div className="w-36 lg:w-44 h-14 rounded-full bg-[#2a2a2a] absolute inset-0 -z-10 translate-x-4 translate-y-4"></div>
+      </button>
     </ElementLayout>
   );
 };
