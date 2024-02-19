59a3c68db2a705df750654d2faec67a989903c97
Jeff-Maina <devyoungblud@gmail.com>
2024-02-19 14:52:34 +0300

    Add button 009

--- src/app/(pages)/elements/buttons/(examples)/button_009/button_009.tsx
+++ src/app/(pages)/elements/buttons/(examples)/button_009/button_009.tsx
@@ -1,73 +1,64 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ElementLayout from "../../../ElementLayout";
 import { codeblock } from "./codeblock";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, useAnimate } from "framer-motion";
 
 const Button009 = () => {
-  const [isButtonClicked, setIsButtonClicked] = useState(false);
-  const toggleState = () => {
-    console.log(isButtonClicked);
-    setIsButtonClicked(!isButtonClicked);
+  const buttonRef = useRef(null);
+  const [scope, animate] = useAnimate();
+
+  const animateBubble = async (el) => {
+    animate(
+      el,
+      {
+        scale: 15,
+        opacity: 0,
+      },
+      {
+        duration: 1,
+      }
+    );
   };
+
+  const addBubbles = ({ clientX, clientY }) => {
+    const parent = buttonRef.current;
+    const bubble = document.createElement("div");
+    bubble.style.height = "30px";
+    bubble.style.width = "30px";
+    bubble.style.position = "absolute";
+    bubble.style.backgroundColor = "#7a7a7a";
+    bubble.style.borderRadius = "50%";
+    bubble.style.opacity = "0.6";
+    bubble.style.zIndex = "4";
+    // Positions;
+    const { top, left, width, height } = parent.getBoundingClientRect();
+
+    const x = clientX - (left + 15);
+    const y = clientY - (top + 15);
+
+    console.log(x, y);
+
+    bubble.style.left = x + "px";
+    bubble.style.top = y + "px";
+    parent.appendChild(bubble);
+
+    animateBubble(bubble);
+
+    setTimeout(() => {
+      // parent.removeChild(bubble);
+    }, 1100);
+  };
+
   return (
     <ElementLayout codeBlock={codeblock}>
       <button
-        onClick={(e) => {
-          toggleState();
-        }}
-        className="bg-zinc-200 rounded-full h-14 md:text-xl px-10"
+        ref={buttonRef}
+        onClick={addBubbles}
+        className="bg-[#2a2a2a] text-white py-4 px-10 rounded-full relative overflow-hidden isolate"
       >
-        <AnimatePresence mode="wait">
-          {isButtonClicked ? (
-            <motion.p
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: 1,
-              }}
-              exit={{
-                opacity: 0,
-              }}
-              transition={{
-                opacity: {
-                  duration: 0.2,
-                },
-              }}
-              className="pointer-events-none"
-            >
-              Settings
-            </motion.p>
-          ) : null}
-        </AnimatePresence>{" "}
-        <AnimatePresence mode="wait">
-          {!isButtonClicked ? (
-            <motion.p
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: 1,
-              }}
-              exit={{
-                opacity: 0,
-                transition: {
-                  opacity: 0,
-                },
-              }}
-              transition={{
-                opacity: {
-                  duration: 0.2,
-                },
-              }}
-              className="pointer-events-none"
-            >
-              Home
-            </motion.p>
-          ) : null}
-        </AnimatePresence>
+        <span className="text-xl relative z-10">click me</span>
       </button>
     </ElementLayout>
   );
