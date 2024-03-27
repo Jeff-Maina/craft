5baa3a45e46426b8a440d6601830c52f05f5cec4
Jeff-Maina <devyoungblud@gmail.com>
2024-03-27 11:18:35 +0300

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 // component imports
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Analytics />
         <Navbar />
         {children}
         <Footer />
